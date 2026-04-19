4693e94cc8bf200c4710380ec10ca1649a023386
IndhujaSadayappan <indhujavs82@gmail.com>
2026-04-19 14:09:50 +0530

    update backend api url to ec2 ip 3

--- backend/server.js
+++ backend/server.js
@@ -16,13 +16,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
 app.use(
   cors({
     origin: ["http://localhost:3000", "http://13.234.18.228:3000"],
     credentials: true,
   })
 );
+
 app.use(cookieParser());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
