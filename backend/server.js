34b85a0f014b2e187e3d9fe927e9be3290caf3bb
IndhujaSadayappan <indhujavs82@gmail.com>
2026-04-19 13:39:17 +0530

    fix: update cors to allow ec2 origin

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    origin: ["http://localhost:3000", "http://13.234.18.228:3000"],
     credentials: true,
   })
 );
